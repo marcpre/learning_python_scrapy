f76cdd62a324fef5957983153b1927ead18808cd
marcpre <marcus.presich@gmail.com>
2018-09-29 06:51:56 +0000

    install scrapy c9

---
+++ fixC9Scrapy.sh
@@ -0,0 +1,15 @@
+pip --version
+sudo pip uninstall scrapy
+sudo pip uninstall twisted
+pip install scrapy --upgrade
+sudo pip install scrapy --upgrade
+sudo apt-get install python-dev
+sudo pip install scrapy --upgrade
+pip install twisted --upgrade
+sudo pip install twisted --upgrade
+scrapy startproject tutorial
+sudo apt-get install python-lxml
+scrapy startproject tutorial
+sudo pip install --upgrade pip
+sudo pip install --upgrade Scrapy
+scrapy startproject tutorial
