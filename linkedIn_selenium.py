b62977b35edd15aa6432d69c8739dc28a7f852a5
marcpre <marcus.presich@gmail.com>
2018-10-16 04:37:21 +0000

    create parameters file

--- linkedIn_selenium.py
+++ linkedIn_selenium.py
@@ -1,3 +1,4 @@
+import parameters
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -6,11 +7,11 @@ driver = webdriver.Chrome('')
 driver.get('http://www.linkedin.com')
 
 username = driver.find_element_by_class_name('login-email')
-username.send_keys('foo@gmail.com')
+username.send_keys(parameters.linkedin_username)
 sleep(0.5)
 
 password = driver.find_element_by_id('login-password')
-password.send_keys('somepass')
+password.send_keys(parameters.linkedin_password)
 sleep(0.5)
 
 sign_in_button = driver.find_element_by_xpath('//*[@type="submit"]')
@@ -18,11 +19,17 @@ sign_in_button.click()
 sleep(5)
 
 driver.get("https://www.google.com")
+sleep(3)
 
 search_query = driver.find_element_by_name('q')
-search_query.send_keys('site:linkedin.com/in/ AND "python developer" AND "London"')
+search_query.send_keys(parameters.search_query)
+sleep(0.5)
 
 search_query.send_keys(Keys.RETURN)
+sleep(3)
 
 linkedin_urls = driver.find_element_by_tag_name('cite')
 linkedin_urls = [url.text for url in linkedin_urls]
+sleep(0.5)
+
+
---
+++ parameters.py
@@ -0,0 +1,6 @@
+search_query = 'site:linkedin.com/in/ AND "python developer" AND "London"'
+file_name = 'results_file.csv'
+
+linkedin_username = ''
+linkedin_password = ''
+
